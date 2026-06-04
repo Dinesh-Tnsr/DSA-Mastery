4a3767eeee5207ff61bb60709e5eeb1ea307892b
Dinesh <dinesh.parupalli.inbox@gmail.com>
2026-06-04 11:24:54 +0530

    feat: implement structural memory allocation and frequency sorting for Huffman tree preparation
    Context: Upgraded the Huffman compression engine from flat array scanning to dynamic memory management.
    
    Execution Architecture:
    - Engineered a HuffmanNode struct to hold character data, frequencies, and binary tree pointers.
    - Implemented dynamic node generation (malloc) to convert raw frequency counts into isolated objects.
    - Applied a sorting algorithm to the node staging array, effectively building the foundational Priority Queue required for the next phase of binary tree assembly.

--- Project-1-Huffman/huffman_engine.c
+++ Project-1-Huffman/huffman_engine.c
@@ -1,5 +1,23 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
+
+struct HuffmanNode{
+    char data;
+    int frq;
+    struct HuffmanNode* left;
+    struct HuffmanNode* right;
+};
+
+struct HuffmanNode* createnode(char value,int freq){
+    struct HuffmanNode* root = malloc(sizeof(struct HuffmanNode));
+    root->data=value;
+    root->frq = freq;
+    root->left = NULL;
+    root->right = NULL;
+
+    return root;
+}
 int main(){
     char text[] = "engineering is about execution";
     int freq[256] = {0};
@@ -16,6 +34,26 @@ int main(){
     for(int i=0;i<256;i++){
         if(freq[i]>0){
             printf("count of '%c': %d\n",i,freq[i]);
+        }
+    }
+
+    struct HuffmanNode* staging[256];
+    int size = 0;
+
+    for(int i=0;i<256;i++){
+        if(freq[i]>0){
+        staging[size] = createnode(i,freq[i]);
+        size++;
+        }
+    }
+
+    for(int i=0;i<=size;i++){
+        for(int j=0;j<=size-1-i;j++){
+            if(staging[j]->frq > staging[ j+1]->frq){
+                struct HuffmanNode* temp = staging[j];
+                staging[j] = staging[j+1];
+                staging[j+1] = temp;
+            }
         }
     }
 
